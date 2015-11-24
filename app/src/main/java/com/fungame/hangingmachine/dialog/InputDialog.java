7d402436037468d916ed0ba440d1f904923a5883
wenjie694@163.com <wenjie694@163.com>
2015-11-25 01:09:30 +0800

    添加事件和接口

---
+++ app/src/main/java/com/fungame/hangingmachine/dialog/InputDialog.java
@@ -0,0 +1,7 @@
+package com.fungame.hangingmachine.dialog;
+
+/**
+ * Created by admin on 15/11/24.
+ */
+public class InputDialog {
+}
---
+++ app/src/main/java/com/fungame/hangingmachine/entity/Const.java
@@ -0,0 +1,7 @@
+package com.fungame.hangingmachine.entity;
+
+/**
+ * Created by admin on 15/11/22.
+ */
+public class Const {
+}
